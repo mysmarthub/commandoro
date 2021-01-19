37311abd935144862d4e7cf14d2e9ec4924bd446
Aleksandr Suvorov <myhackband@gmail.com>
2021-01-19 14:05:50 +0700

    Fix errors

--- commandoro/commandoro.py
+++ commandoro/commandoro.py
@@ -21,7 +21,7 @@ __title__ = 'Commandoro'
 __version__ = '0.0.7'
 __author__ = 'Aleksandr Suvorov'
 __description__ = 'CLI utility for automatic command execution'
-__url__ = 'https://githib.com/mysmarthub'
+__url__ = 'https://github.com/mysmarthub'
 __donate__ = 'Donate: https://yoomoney.ru/to/4100115206129186'
 __copyright__ = 'Copyright © 2020-2021 Aleksandr Suvorov'
 
